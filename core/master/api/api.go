824f2781012dfe743cf0c42d7689eaad32c3d793
Seth Hoenig <seth.a.hoenig@gmail.com>
2016-05-01 11:31:17 -0500

    move more api things around

--- core/master/api/api.go
+++ core/master/api/api.go
@@ -5,31 +5,10 @@ package api
 import (
 	"encoding/json"
 	"net/http"
-	"time"
 
-	"github.com/gorilla/mux"
 	"github.com/shoenig/subspace/core/common/stream"
 	"github.com/shoenig/subspace/core/master/state"
 )
-
-// Server returns an http.Server serving up the API.
-func Server(address string, store state.Store) *http.Server {
-	return &http.Server{
-		Addr:         address,
-		Handler:      router(store),
-		ReadTimeout:  30 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
-}
-
-func router(store state.Store) *mux.Router {
-	r := mux.NewRouter()
-	api := NewAPI(store)
-	r.HandleFunc("/v1/streams", api.AllStreams).Methods("GET")
-	r.HandleFunc("/v1/streams/create", api.NewStream).Methods("PUT")
-	r.HandleFunc("/v1/streams/publish", api.PublishGeneration).Methods("POST")
-	return r
-}
 
 // API is the api handler for a master.
 type API struct {
